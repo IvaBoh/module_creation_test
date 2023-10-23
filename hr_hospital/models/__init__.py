4406bcf1d8f8b172ac1bdec751b82e05341d244b
Iva Boh <antutilo@gmail.com>
2023-10-23 13:48:17 +0300

    [IMP] hr_hospital: __init__.py formatting change.

--- hr_hospital/models/__init__.py
+++ hr_hospital/models/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import hospital_disease, hospital_patient, hospital_doctor, hospital_patient_visit
+from . import (
+    hospital_disease,
+    hospital_patient,
+    hospital_doctor,
+    hospital_patient_visit,
+)
