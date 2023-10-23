820d6c87f6271c4910d4f399303d217205bd4c82
Iva Boh <antutilo@gmail.com>
2023-10-23 14:07:33 +0300

    [IMP] hr_hospital: models formatting are done.

--- hr_hospital/models/hospital_patient.py
+++ hr_hospital/models/hospital_patient.py
@@ -6,5 +6,7 @@ class HospitalPatient(models.Model):
     _description = "Patient"
 
     name = fields.Char(string="Name", required=True)
-    attending_doctor_id = fields.Many2one("hospital.doctor", string="Attending Doctor")
+    attending_doctor_id = fields.Many2one(
+        "hospital.doctor", string="Attending Doctor"
+    )
     # Add other fields as needed
--- hr_hospital/models/hospital_patient_visit.py
+++ hr_hospital/models/hospital_patient_visit.py
@@ -5,8 +5,12 @@ class HospitalPatientVisit(models.Model):
     _name = "hospital.patient_visit"
     _description = "Patient Visits"
 
-    patient_id = fields.Many2one("hospital.patient", string="Patient", required=True)
-    doctor_id = fields.Many2one("hospital.doctor", string="Doctor", required=True)
+    patient_id = fields.Many2one(
+        "hospital.patient", string="Patient", required=True
+    )
+    doctor_id = fields.Many2one(
+        "hospital.doctor", string="Doctor", required=True
+    )
     diagnosed_disease_ids = fields.Many2many(
         "hospital.disease", string="Diagnosed Diseases"
     )
