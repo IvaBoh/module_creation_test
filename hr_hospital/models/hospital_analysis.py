e1276635ac536497bc817c0627de818b4cc80ef1
Iva Boh <antutilo@gmail.com>
2023-11-07 22:03:41 +0200

    [IMP] hr_hospital: week 4 - all tasks done.

--- hr_hospital/models/hospital_analysis.py
+++ hr_hospital/models/hospital_analysis.py
@@ -15,16 +15,33 @@ class HospitalAnalysis(models.Model):
             ("soft_tissue", "Soft tissue"),
         ],
     )
+    analysis_date = fields.Date(
+        required=False,
+    )
     patient_id = fields.Many2one(
         comodel_name="hospital.patient",
         required=False,
     )
+    physician_id = fields.Many2one(
+        comodel_name="hospital.physician",
+        string="Computed Physician",
+        compute="_compute_physician_id",
+        store=True,
+    )
+
+    @api.depends("patient_id")
+    def _compute_physician_id(self):
+        for record in self:
+            if record.patient_id:
+                record.physician_id = record.patient_id.physician_id
+            else:
+                record.physician_id = False
 
     def name_get(self):
         return [
             (
                 record.id,
-                (record.title),
+                record.title,
             )
             for record in self
         ]
